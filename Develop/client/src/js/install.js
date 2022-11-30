f25da941b70358e5bbb82f86fc89d8ebdb17bc18
Mitchell Foote <mcfoote321@gmail.com>
2022-11-30 03:22:34 -0500

    PWA installation logic

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -12,7 +12,17 @@ window.addEventListener('beforeinstallprompt', (event) => {
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-    
+
+    const promptEvent = window.defferedPrompt;
+
+    if(!promptEvent) {
+        return;
+    }
+
+    promptEvent.prompt();
+    window.deferredPrompt = null;
+    butInstall.classList.toggle('hidden', true);
+
 });
 
 // TODO: Add an handler for the `appinstalled` event
